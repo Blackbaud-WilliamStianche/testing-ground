3daad7997ebea9ff0b9ebdf856139d32fb15c5cc
William Stianche <William.Stianche@blackbaud.com>
2015-08-16 21:36:32 -0500

    2

--- get-jvm-procinfo.py
+++ get-jvm-procinfo.py
@@ -7,6 +7,7 @@ import json
 import os
 import subprocess
 
-psj = subprocess.call("ps", "-fC")
+process_name = "Xorg"
+psj = subprocess.call(["ps","-fC",process_name])
 for process in iter(psj.stdout.readline, ''):
     print process,
